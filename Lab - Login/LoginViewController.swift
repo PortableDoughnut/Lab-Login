ae8ea914ff47c0af1dd2d982214446aff22562c4
PortableDoughnut <gwen.thelin@gmail.com>
2024-09-18 10:06:37 -0600

    Added Navigation Controller tweaks and a prepare for segue

--- Lab - Login/LoginViewController.swift
+++ Lab - Login/LoginViewController.swift
@@ -7,7 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var loginInButton: UIButton!
     
     override func viewDidLoad() {
@@ -16,7 +18,10 @@ class ViewController: UIViewController {
         loginInButton.layer.cornerRadius = 17
         loginInButton.clipsToBounds = true
     }
-
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+    
 }
 
